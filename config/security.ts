62880aa84defc6cecd9dd3fc7737544b41cf754c
Kamil Dehliwala <walakamil26@gmail.com>
2025-02-14 16:16:16 +0530

    front end url changed

--- config/security.ts
+++ config/security.ts
@@ -1,10 +1,10 @@
 module.exports = {
   cors: {
     enabled: true,
-    origin: ['http://localhost:5173'],
+    origin: ["http://localhost:5173", "https://my-chat-mz9q.onrender.com/"],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-    headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+    headers: ["Content-Type", "Authorization", "Origin", "Accept"],
     keepHeaderOnError: true,
   },
   csrf: {
--- config/server.ts
+++ config/server.ts
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  url: env("PUBLIC_URL", "http://localhost:1337"),
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
   cors: {
     enabled: true,
-    origin: ['http://localhost:5173'], // Add your frontend URL
+    origin: ["http://localhost:5173", "https://my-chat-mz9q.onrender.com/"],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-    headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+    headers: ["Content-Type", "Authorization", "Origin", "Accept"],
   },
 });
