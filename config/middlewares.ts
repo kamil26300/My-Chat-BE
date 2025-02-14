80f44a3304869ecb346e57816bfc8b6d4a13143f
Kamil Dehliwala <walakamil26@gmail.com>
2025-02-14 16:21:57 +0530

    cors frontend fixed

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -35,7 +35,7 @@ module.exports = [
       },
       cors: {
         enabled: true,
-        origin: ["http://localhost:5173", "https://my-chat-mz9q.onrender.com/"], // Add your frontend URL
+        origin: ["http://localhost:5173", "https://my-chat-mz9q.onrender.com"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
         headers: ["Content-Type", "Authorization", "Origin", "Accept"],
--- config/server.ts
+++ config/server.ts
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
   },
   cors: {
     enabled: true,
-    origin: ["http://localhost:5173", "https://my-chat-mz9q.onrender.com/"],
+    origin: ["http://localhost:5173", "https://my-chat-mz9q.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
     headers: ["Content-Type", "Authorization", "Origin", "Accept"],
